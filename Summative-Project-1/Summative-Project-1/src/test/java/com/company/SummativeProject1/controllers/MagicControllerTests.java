a9e34494be47cba71a747cffe6f3ecb2f82be68d
liannnaa <lnp2016@nyu.edu>
2023-06-29 18:12:56 -0500

    added template for magic controller tests

--- Summative-Project-1/Summative-Project-1/src/test/java/com/company/SummativeProject1/controllers/MagicControllerTests.java
+++ Summative-Project-1/Summative-Project-1/src/test/java/com/company/SummativeProject1/controllers/MagicControllerTests.java
@@ -1,5 +1,8 @@
 package com.company.magic8ball.controllers;
 
+@WebMvcTest(MagicController.class)
 public class MagicControllerTests {
-
+    @Autowired
+    private MockMvc mockMvc;
+    private ObjectMapper mapper = new ObjectMapper();
 }
