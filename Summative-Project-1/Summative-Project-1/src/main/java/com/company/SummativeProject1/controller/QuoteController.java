4d01a9c1d18c1a08f8d197cfda842fa11a09d946
Nathalie Pacheco <105689373+natsola@users.noreply.github.com>
2023-06-29 18:51:40 -0400

    Add files via upload

---
+++ Summative-Project-1/Summative-Project-1/src/main/java/com/company/SummativeProject1/controller/QuoteController.java
@@ -0,0 +1,16 @@
+package com.company.SummativeProject1.controller;
+
+import com.company.SummativeProject1.model.Quote;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class QuoteController {
+    private List<Quote> quoteList = new ArrayList<>();
+    int idCounter;
+    idCounter ++
+
+
+}
---
+++ Summative-Project-1/Summative-Project-1/src/main/java/com/company/SummativeProject1/model/Quote.java
@@ -0,0 +1,38 @@
+package com.company.SummativeProject1.model;
+
+
+public class Quote {
+    private int id;
+    private String author;
+    private String quote;
+
+    public Quote(String author, String quote, int id ){
+        this.author = author;
+        this.quote = quote;
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getQuote() {
+        return quote;
+    }
+
+    public void setQuote(String quote) {
+        this.quote = quote;
+    }
+}
