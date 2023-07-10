19160619fc459c1642475d4ddc28a3c3a4cb5ffb
Liliana Que <90230601+lilianaque@users.noreply.github.com>
2023-07-09 20:02:20 -0400

    Add files via upload

---
+++ Summative-Project-1/Summative-Project-1/src/main/java/com/company/SummativeProject1/model/Definition.java
@@ -0,0 +1,41 @@
+package com.company.SummativeProject1.model;
+
+public class Definition {
+    private int id;
+    private String word;
+    private String definition;
+
+    //constructor
+    public Definition(int id, String word, String definition){
+        this.id = id;
+        this.word = word;
+        this.definition = definition;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setQuestion(String word) {
+        this.word = word;
+    }
+
+    public String getDefinition() {
+        return definition;
+    }
+
+    public void setDefinition(String definition) {
+        this.definition = definition;
+    }
+}
+
+
+
